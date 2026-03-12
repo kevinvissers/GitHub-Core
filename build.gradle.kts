198f58b1fc41a195af49610124e9d08b6d64134d
Kevin Vissers <5878861+kevinvissers@users.noreply.github.com>
2026-03-12 21:04:51 +0100

    build and test

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
